699189691d2614c0ddeacfcd2a7294a123ddd5fb
Nelson wafula <34085153+Bilmage@users.noreply.github.com>
2023-08-22 10:56:12 +0000

    update

--- src/components/BusinessCard/BusinessCard.js
+++ src/components/BusinessCard/BusinessCard.js
@@ -1,65 +1,94 @@
 import React from "react";
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import Safaricom from "../../assets/images/Safaricom.png";
-import Certified from "../../assets/icons/Certified.png";
-import Heart from "../../assets/icons/Heart.svg";
-import Button from "../Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faBookmark } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 import "./business.css";
 
-export const BusinessCard = () => {
-    return (
-        <div className="business-card">
-            <div className="liked">
-                <div className="user-liked">
-                    <div className="ratings">
-                        <faStar />
-                        <faStar />
-                        <faStar />
-                        <faStar />
-                        <div className="text-wrapper">4 of 5</div>
-                    </div>
-                </div>
+import SafaricomImage from "../../assets/images/Safaricom.png";
+import CertifiedIcon from "../../assets/icons/Certified.png";
+import HeartIcon from "../../assets/icons/Heart.svg";
+
+const businessData = [
+  {
+    name: "Safaricom",
+    type: "Business",
+    imageSrc: SafaricomImage,
+    description:
+      "It is the largest telecommunications provider in Kenya, and one of the most profitable companies in the East and Central Africa region.",
+    location: "Nairobi, Westlands",
+    isNew: true,
+    isCertified: true,
+    rating: 4,
+  },
+  // Add more business entries if needed
+];
+
+const BusinessCard = ({ business }) => {
+  return (
+    <div className="container">
+      <div className="business-card">
+        <div className="liked">
+          <div className="user-liked">
+            <div className="ratings">
+              {Array.from({ length: business.rating }).map((_, index) => (
+                <FontAwesomeIcon key={index} icon={faStar} className="star-icon" />
+              ))}
+              <div className="text-wrapper">{business.rating} of 5</div>
             </div>
-            <div className="body">
-                <img className="image" alt="Img" src={Safaricom} />
-                <div className="frame">
-                    <div className="div">
-                        <div className="text-wrapper-2">Safaricom</div>
-                        <div className="text-wrapper-3">Business</div>
-                        <div className="new">
-                            <img className="heart" alt="Heart" src={Heart} />
-                            <div className="text-wrapper-4">New</div>
-                        </div>
-                    </div>
-                    <div className="description">
-                        <p className="p">
-                            It is the largest telecommunications provider in Kenya, and one of the most profitable companies in the
-                            East and Central Africa region.
-                        </p>
-                    </div>
-                    <div className="div">
-                        <div className="text-wrapper-5">Nairobi, Westlands</div>
-                    </div>
-                </div>
-            </div>
-            <div className="certified">
-                <div className="certified-logo">
-                    <img className="element" alt="Element" src={Certified} />
-                    <div className="text-wrapper-6">Certified</div>
-                </div>
-            </div>
-            <div className="call-to-action">
-                <Link to="#">
-                    <Button text="Leave a review" icon={faStar}></Button>
-                </Link>
-                <div className="state-pressed-with">
-                    <Link to="#">
-                        <Button text="Save for later" icon={faStar}></Button>
-                    </Link>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+        <div className="body">
+          <img className="image" alt="Img" src={business.imageSrc} />
+          <div className="frame">
+            <div className="div">
+              <div className="text-wrapper-2">{business.name}</div>
+              <div className="text-wrapper-3">{business.type}</div>
+              {business.isNew && (
+                <div className="new">
+                  <img className="heart" alt="Heart" src={HeartIcon} />
+                  <div className="text-wrapper-4">New</div>
+                </div>
+              )}
+            </div>
+            <div className="description">
+              <p className="p">{business.description}</p>
+            </div>
+            <div className="div">
+              <div className="text-wrapper-5">{business.location}</div>
+            </div>
+          </div>
+        </div>
+        {business.isCertified && (
+          <div className="certified">
+            <div className="certified-logo">
+              <img className="element" alt="Element" src={CertifiedIcon} />
+              <div className="text-wrapper-6">Certified</div>
+            </div>
+          </div>
+        )}
+        <div className="call-to-action">
+          <Link to="#">
+            <Button text="Leave a review" icon={faStar}></Button>
+          </Link>
+          <Link to="#">
+            <Button text="Save for later" icon={faBookmark}></Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default BusinessCard;
+
+const BusinessList = () => {
+  return (
+    <div>
+      {businessData.map((business, index) => (
+        <BusinessCard key={index} business={business} />
+      ))}
+    </div>
+    
+  );
+};
+
+export default BusinessList;
